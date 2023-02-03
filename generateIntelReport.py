1f5d0b3d38e6ac70b55c7c5f64ae454b1981b371
Drew Kirkpatrick <test@tester.com>
2023-02-03 13:46:01 -0500

    Improvements to report generator

--- generateIntelReport.py
+++ generateIntelReport.py
@@ -16,34 +16,34 @@ def printHeader():
 
 def create_title(title, pdf):
 
-    # Add main title
-    pdf.set_font('Helvetica', 'b', 20)  
-    pdf.ln(40)
-    pdf.write(5, title)
-    pdf.ln(10)
-    
-    # Add date of report
-    pdf.set_font('Helvetica', '', 14)
-    pdf.set_text_color(r=128,g=128,b=128)
-    today = time.strftime("%d/%m/%Y")
-    pdf.write(4, "Report Generated: " + f'{today}')
-    
-    # Add line break
-    pdf.ln(10)
+	# Add main title
+	pdf.set_font('Helvetica', 'b', 20)  
+	pdf.ln(40)
+	pdf.write(5, title)
+	pdf.ln(10)
+	
+	# Add date of report
+	pdf.set_font('Helvetica', '', 14)
+	pdf.set_text_color(r=128,g=128,b=128)
+	today = time.strftime("%d/%m/%Y")
+	pdf.write(4, "Report Generated: " + f'{today}')
+	
+	# Add line break
+	pdf.ln(10)
 
 
 def write_to_pdf(pdf, words, bold):
-    # Set text colour, font size, and font type
-    pdf.set_text_color(r=0,g=0,b=0)
-    if (bold):
-    	pdf.set_font('Helvetica', 'b', 12)
+	# Set text colour, font size, and font type
+	pdf.set_text_color(r=0,g=0,b=0)
+	if (bold):
+		pdf.set_font('Helvetica', 'b', 12)
 	else:
 		pdf.set_font('Helvetica', '', 12)
-    
-    # pdf.set_font('Helvetica', '', 12)
-    
-    pdf.write(5, words)
-    pdf.ln(6)
+	
+	# pdf.set_font('Helvetica', '', 12)
+	
+	pdf.write(5, words)
+	pdf.ln(6)
 
 
 
@@ -108,21 +108,31 @@ def readSession():
 					# # cleanTime = datetime.datetime.fromtimestamp(int(intTime))
 					print("Cleaned up time: " + date_conv)
 					writeLine = date_conv + " UTC:"
-					write_to_pdf(pdf, writeLine, false)
+					pdf.ln(5)
+					write_to_pdf(pdf, writeLine, True)
 
 					# Ok, now handle the event type:
 					eventType = splitLine[1]
 					print("Got event type: " + eventType)
 
-					match eventType:
-						case "URL Visited":
-						case "Cookie Name":
-						case "Local Storage Entry":
-						case "Screenshot":
-						case _:
-							print("ERROR: Unhandled event type in generator")
-							
-					write_to_pdf(pdf, eventType, true);
+					if (eventType == "URL Visited"):
+						write_to_pdf(pdf, eventType + "\n" + splitLine[2], False)
+					elif (eventType == "User input field"):
+						write_to_pdf(pdf, eventType + "\n" + splitLine[2], False)
+					elif (eventType == "Cookie Name"):
+						write_to_pdf(pdf, eventType + "\n" + splitLine[2], False)
+					elif (eventType == "Local Storage Entry"):
+						write_to_pdf(pdf, eventType + "\n" + splitLine[2], False)
+					elif (eventType == "Screenshot"):
+						write_to_pdf(pdf, eventType, False)
+						imageFile = path + "/" + splitLine[2]
+						imageFile = imageFile[:-1]
+						pdf.image(imageFile, w=170)
+						#pdf.image("./loot/client_1/1_Screenshot.png", w=170)
+
+						pdf.ln(10)
+					else:
+						print("ERROR: Unhandled event type in generator")
 
 
 
@@ -136,26 +146,26 @@ def readSession():
 
 			return
 
-	# Generate PDF
-	Title = "TrustedSec Intel Report"
+	# # Generate PDF
+	# Title = "TrustedSec Intel Report"
 
-	# A4 size in mm
-	Width = 210
-	Height = 297
+	# # A4 size in mm
+	# Width = 210
+	# Height = 297
 
-	pdf = FPDF()
+	# pdf = FPDF()
 
-	# First page
-	pdf.add_page()
-	create_title(Title, pdf)
-
-
-	write_to_pdf(pdf, "TrustedSec Intel Report")
-	pdf.image("./loot/client_1/1_Screenshot.png", w=170)
-	pdf.ln(15)
+	# # First page
+	# pdf.add_page()
+	# create_title(Title, pdf)
 
 
-	pdf.output("./testReport.pdf", 'F')
+	# write_to_pdf(pdf, "TrustedSec Intel Report")
+	# pdf.image("./loot/client_1/1_Screenshot.png", w=170)
+	# pdf.ln(15)
+
+
+	# pdf.output("./testReport.pdf", 'F')
 
 
 
