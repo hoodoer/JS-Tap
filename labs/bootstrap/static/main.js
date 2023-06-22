f8e1340721e9459953192e649f0b779445ef0d2f
hoodoer <drew.kirkpatrick@gmail.com>
2023-06-21 22:24:04 -0400

    Starting to play with jquery and bootstrap-tables, still broken

--- labs/bootstrap/static/main.js
+++ labs/bootstrap/static/main.js
@@ -11,6 +11,22 @@ function getClients()
 	req.onload  = function() {
 	   var jsonResponse = req.response;
 	   console.log(JSON.stringify(jsonResponse));
+
+
+	   $(document).ready(function() {
+	   	$('table').bootstrapTable({
+	   		data: clientData
+	   	});
+	   });
+
+	   var clientData = 
+	   [
+	   {
+	   	"id": "test",
+	   	"nickname": "testNickname",
+	   	"lastSeen": "42 seconds"
+	   }];
+
 	   // do something with jsonResponse
 	};
 	req.send(null);
